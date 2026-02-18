0dab5a9fbb7b7d6913050cb6861791ea00fc2692
ekagra <ekagrashharma@gmail.com>
2026-02-18 21:45:57 +0530

    add function to validate user fields and check for emptiness

--- javascript/array/every.js
+++ javascript/array/every.js
@@ -2,3 +2,13 @@ const fields = [
   {name:"email",value:"abc@mail.com" },
   {name:"password",value:"" }
 ]
+
+
+function isUserIsempty(fields){
+
+  const emptyField  = fields.every((field) =>( field.value !== "" ))
+  return emptyField ? "fields are valid" : "fields are empty"
+
+}
+
+console.log(isUserIsempty(fields))
