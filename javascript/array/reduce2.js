95a8034e309610217a58ee0fa50b80ff3f548661
ekagra <ekagrashharma@gmail.com>
2026-02-19 21:33:05 +0530

    add function to find duplicate users based on email

--- javascript/array/reduce2.js
+++ javascript/array/reduce2.js
@@ -3,3 +3,19 @@ const users = [
   {email:"b@mail.com" },
   {email:"a@mail.com" }
 ]
+
+
+
+function findDuplicateUsers(users) {
+  const emailCount = {};
+
+  // Count emails
+  users.forEach(user => {
+    emailCount[user.email] = (emailCount[user.email] || 0) + 1;
+  });
+
+  // Filter users having duplicate email
+  return users.filter(user => emailCount[user.email] > 1);
+}
+
+console.log(findDuplicateUsers(users));
