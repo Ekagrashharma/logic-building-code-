a545d87e4fe16fed57030b98ecae9c1e1fd972cd
ekagra <ekagrashharma@gmail.com>
2026-02-19 22:27:18 +0530

    react basic

---
+++ react/logic-building/response/response1.js
@@ -0,0 +1,5 @@
+export const users = [
+  {id:1,name:"Aman",role:"admin",active:true },
+  {id:2,name:"Sara",role:"editor",active:false },
+  {id:3,name:"John",role:"viewer",active:true }
+];
--- react/logic-building/src/App.jsx
+++ react/logic-building/src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+const employees = [ 
+    {id:1,name:"Aman" }, 
+    {id:2,name:"Sara" } 
+  ];
 
-import React from 'react'
+function showEmployees(){
+  return(
+    <ul>
+      {employees.map((employee)=>(
+        <li key={employee.id}>{employee.name}</li>
+      ))}
+    </ul>
+  )
+}
+
 
 const App = () => {
   return (
     <div>
-      
+      {showEmployees()}
     </div>
   )
 }
