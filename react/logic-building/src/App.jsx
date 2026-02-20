d63ca0442d1eeb091b84b8876df606f5033f58c9
ekagra <ekagrashharma@gmail.com>
2026-02-20 22:31:13 +0530

    refactor: extract Usercard component for better user rendering

--- react/logic-building/src/App.jsx
+++ react/logic-building/src/App.jsx
@@ -6,9 +6,21 @@ const employees = [
 
 const isLoggedIn =false;
 
-const App = () => {
+function Usercard({user}){
   return (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Role:{user.role}</p>
+  </div>)
+}
+
+const App = () => {
+
+    const user = {name:"John",role:"Admin" };
+
+    return (
     <div>
+      <Usercard user={user}/>
       <p>{isLoggedIn ? "user is  login , Welcome back " : "user is not logged in , please first login"}</p>
       <ul>
       {employees.map((employee)=>(
