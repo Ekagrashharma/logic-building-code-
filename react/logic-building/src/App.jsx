ccd6b289ff92844bafb1f21267f88b46da2747fc
ekagra <ekagrashharma@gmail.com>
2026-02-20 22:15:31 +0530

    feat : loginuser

--- react/logic-building/src/App.jsx
+++ react/logic-building/src/App.jsx
@@ -4,11 +4,12 @@ const employees = [
     {id:2,name:"Sara" } 
   ];
 
-
+const isLoggedIn =false;
 
 const App = () => {
   return (
     <div>
+      <p>{isLoggedIn ? "user is  login , Welcome back " : "user is not logged in , please first login"}</p>
       <ul>
       {employees.map((employee)=>(
         <li key={employee.id}>{employee.name}</li>
