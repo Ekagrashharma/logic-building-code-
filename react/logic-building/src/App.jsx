9dfa082ee4f638e41106ea2e2605a60edbfc3929
ekagra <ekagrashharma@gmail.com>
2026-02-21 22:44:54 +0530

    notification checker

--- react/logic-building/src/App.jsx
+++ react/logic-building/src/App.jsx
@@ -1,35 +1,45 @@
 import './App.css'
-import Laptop from './components/Laptop';
-const employees = [ 
-    {id:1,name:"Aman" }, 
-    {id:2,name:"Sara" } 
-  ];
+// import Laptop from './components/Laptop';
+// const employees = [ 
+//     {id:1,name:"Aman" }, 
+//     {id:2,name:"Sara" } 
+//   ];
 
   
-const isLoggedIn =false;
+// const isLoggedIn =false;
 
-function Usercard({user}){
-  return (
-  <div>
-    <h1>{user.name}</h1>
-    <p>Role:{user.role}</p>
-  </div>)
-}
+// function Usercard({user}){
+//   return (
+//   <div>
+//     <h1>{user.name}</h1>
+//     <p>Role:{user.role}</p>
+//   </div>)
+// }
+// const user = {name:"John",role:"Admin" };
+// const product = {name:"Laptop",price:50000 };
+const notification = [];
+// function Isnotificaion(notification){
+//     return (!notification? "you have notifications" : "you have zero notification")
+// }
 
 const App = () => {
 
-    const user = {name:"John",role:"Admin" };
-const product = {name:"Laptop",price:50000 };
     return (
-      <div>
-      <Laptop product={product}/>
-      <Usercard user={user}/>
-      <p>{isLoggedIn ? "user is  login , Welcome back " : "user is not logged in , please first login"}</p>
-      <ul>
-      {employees.map((employee)=>(
-        <li key={employee.id}>{employee.name}</li>
-      ))}
-    </ul>
+    //   <div>
+    //   <Laptop product={product}/>
+    //   <Usercard user={user}/>
+    //   <p>{isLoggedIn ? "user is  login , Welcome back " : "user is not logged in , please first login"}</p>
+    //   <ul>
+    //   {employees.map((employee)=>(
+    //     <li key={employee.id}>{employee.name}</li>
+    //   ))}
+    // </ul>
+    // </div>
+    <div>
+      <p>{ notification.length > 0 
+      ? "you have notications" 
+      : "you have zero notification"
+      }</p>
     </div>
   )
 }
