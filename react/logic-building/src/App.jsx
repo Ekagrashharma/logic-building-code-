6b1f4b7af3d7145c75c7e2f90ba56e81dfb08e40
ekagra <ekagrashharma@gmail.com>
2026-03-20 20:56:20 +0530

    feat: refactor App component into toggleRole component for role management and theme context

--- react/logic-building/src/App.jsx
+++ react/logic-building/src/App.jsx
@@ -1,36 +1,10 @@
-import {use, useContext, useState} from 'react'
 import './App.css'
-import Dashboard from './components/Dashboard'
-import { ThemeContext } from './components/themeContext'
 
-// const user= {name:"Aman",role:"admin"}
-
-const themes= {
- admin:{color:"red"},
- editor:{color:"blue"},
- viewer:{color:"green"}
-}
 const App = () => {
-  const [user, setuser] = useState({name:"Aman",role:"user"})
-
-  const theme = themes[user.role]
-
-  const changerole =(role)=>{
-    setuser({...user, role})
-  }
-
   return (
-    <ThemeContext.Provider value={theme}>
     <div>
-      <h1>User :{user.name}</h1>
-
-      <button onClick={()=>{changerole("admin")}} >Admin</button>
-      <button onClick={()=>{changerole("editor")}} >Editor</button>
-      <button onClick={()=>{changerole("user")}} >User</button>
-
-    <Dashboard />
+      hello
     </div>
-    </ThemeContext.Provider>
   )
 }
 
